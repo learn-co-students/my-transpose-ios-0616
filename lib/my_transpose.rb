5010b631b95a89fb7119af2769fdb1d8b3ad2d86
Katie Hoffman <ktahoffman@gmail.com>
2015-03-31 17:53:36 -0400

    add solution

--- lib/my_transpose.rb
+++ lib/my_transpose.rb
@@ -1,2 +1,17 @@
 # your code goes here
 # you will be adding the method #my_group_by to the Array class
+
+
+class Array
+
+  def my_transpose
+    result = Array.new(self[0].length)
+    self.each do |array|
+      array.each_with_index do |element, i|
+        result[i] ||= []
+        result[i] << element
+      end
+    end
+    result
+  end
+end
--- spec/my_transpose_spec.rb
+++ spec/my_transpose_spec.rb
@@ -1,29 +1,26 @@
 describe "#my_group_by" do
-
-  let(:numbers) { [[1,2], [3,4], [5,6]] }
-  let(:alphabet) { [["a", "d", "g"], ["b", "e", "h"], ["c", "f", "i"]] }
-  let(:letters) { [[:a, :c, :e, :g], [:b, :d, :f, :h]] }
-
+  
   it "doesn't change the original array" do
+    numbers = [[1,2], [3,4], [5,6]]
     numbers.my_transpose
     expect(numbers).to eq(numbers)
   end
 
   it "doesn't use #transpose" do     
     expect_any_instance_of(Array).to_not receive(:transpose)
-    numbers.my_transpose
+    [[1,2], [3,4], [5,6]].my_transpose
   end
 
   it "groups three nested arrays of two elements into two nested arrays of three elements" do
-    expect(numbers.my_transpose).to eq([[1, 3, 5], [2, 4, 6]])
+    expect([[1,2], [3,4], [5,6]].my_transpose).to eq([[1, 3, 5], [2, 4, 6]])
   end
 
   it "groups three nested arrays of three elements into three nested arrays of three elements" do
-    expect(alphabet.my_transpose).to eq([["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]])
+    expect([["a", "d", "g"], ["b", "e", "h"], ["c", "f", "i"]].my_transpose).to eq([["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]])
   end
 
   it "groups two nested arrays of four elements into four nested arrays of two elements" do
-    expect(letters.my_transpose).to eq([[:a, :b], [:c, :d], [:e, :f], [:g, :h]])
+    expect([[:a, :c, :e, :g], [:b, :d, :f, :h]].my_transpose).to eq([[:a, :b], [:c, :d], [:e, :f], [:g, :h]])
   end
 
 end
